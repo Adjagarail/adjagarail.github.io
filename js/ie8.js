031f5ede2c2f577904bda1f9d16c4ff3ac2c6c84
SADIO <adjagarail@gmail.com>
2021-02-19 11:42:09 +0100

    First commit

---
+++ js/ie8.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function(e) {
+    jQuery('body').html('<div class="ie8"><span>Salut! Veuillez utiliser la dernière version du navigateur pour visiter la page actuelle</span></div>')
+});
